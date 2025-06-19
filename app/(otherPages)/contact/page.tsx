acf3d5d9aaf83ced08ec137d621748b48452e1ef
Ibrahim Odesola <tocyberDevv@gmail.com>
2025-06-19 14:00:55 +0100

    Refactor code structure for improved readability and maintainability

--- app/(otherPages)/contact/page.tsx
+++ app/(otherPages)/contact/page.tsx
@@ -1,9 +1,110 @@
-import React from 'react'
+"use client";
+
+import Location from "@/components/home/Location";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+import Image from "next/image";
+import React from "react";
+
+type InputLabelProps = {
+  label: string;
+  value: string;
+  setValue: (value: string) => void;
+  className?: string;
+  isTextArea?: boolean;
+  type?: React.HTMLInputTypeAttribute;
+};
+
+const InputLabel: React.FC<InputLabelProps> = ({
+  label,
+  value,
+  setValue,
+  className,
+  isTextArea,
+  type = "text",
+}) => {
+  return (
+    <div className={`grid w-full max-w-sm items-center gap-3 ${className}`}>
+      <Label
+        className="font-lato text-[#41444B] ~text-[0.938rem]/[1.063rem] leading-normal xl:leading-[28px] font-medium"
+        htmlFor={label}
+      >
+        {label}
+      </Label>
+      {!isTextArea ? (
+        <Input
+          type={type}
+          id={label}
+          placeholder={label}
+          className="font-lato text-[#41444B] ~text-[0.938rem]/[1.063rem] leading-normal xl:leading-[28px]"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+      ) : (
+        <Textarea
+          placeholder={label}
+          rows={5}
+          className="font-lato text-[#41444B] ~text-[0.938rem]/[1.063rem] leading-normal xl:leading-[28px]"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+      )}
+    </div>
+  );
+};
 
 const Page = () => {
-  return (
-    <div>Page</div>
-  )
-}
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [phone, setPhone] = React.useState("");
+  const [address, setAddress] = React.useState("");
+  const [message, setMessage] = React.useState("");
 
-export default Page
+  return (
+    <div>
+      <section className="container_fluid ~py-[3rem]/[5rem]">
+        <form className="grid gap-10 lg:grid-cols-2 max-w-screen-sm mx-auto">
+          <InputLabel label="Name" value={name} setValue={setName} />
+          <InputLabel
+            label="Email"
+            value={email}
+            setValue={setEmail}
+            type="email"
+          />
+          <InputLabel
+            label="Phone Number"
+            value={phone}
+            setValue={setPhone}
+            type="tel"
+          />
+          <InputLabel label="Address" value={address} setValue={setAddress} />
+          <InputLabel
+            label="Message"
+            value={message}
+            setValue={setMessage}
+            className="lg:col-span-2 w-full max-w-none"
+            isTextArea
+          />
+          <Button
+            variant="secondary"
+            className="py-5 px-16 h-auto ~mt-2/4 lg:!w-2/3"
+          >
+            Submit Message
+            <Image
+              src="/svg/arrow-right.svg"
+              className="ml-[5px]"
+              width={13}
+              height={14}
+              alt="arrow-right"
+            />
+          </Button>
+        </form>
+      </section>
+      <Location />
+    </div>
+  );
+};
+
+export default Page;

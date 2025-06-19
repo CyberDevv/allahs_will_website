67828de51995ab4f15f3341d7a97c7b8581f454e
Ibrahim Odesola <tocyberDevv@gmail.com>
2025-06-19 06:27:46 +0100

    Enhance ImageGallery component with responsive navigation arrows for better user experience

--- components/portfolio/ImageGallery.tsx
+++ components/portfolio/ImageGallery.tsx
@@ -56,9 +56,11 @@ export default function ImageGallery({
 
         {modalOpen && images.length > 0 && (
           <ModalBody className="flex flex-col items-center justify-center !max-w-screen-xl">
-            <div className="between px-4 py-5 w-full">
+            <div className="flex items-center justify-center w-full">
+              {/* Desktop: arrows outside image, Mobile: arrows on image */}
+              {/* Left Arrow */}
               <button
-                className="text-black text-5xl mr-4 z-10 p-2 hover:bg-black/30 rounded-full"
+                className="hidden sm:block text-black text-5xl mr-4 z-10 p-2 hover:bg-black/30 rounded-full"
                 onClick={prevImage}
                 aria-label="Previous"
                 type="button"
@@ -66,16 +68,34 @@ export default function ImageGallery({
                 &#8592;
               </button>
               <div className="relative w-[95vw] max-w-5xl aspect-[4/3] rounded overflow-hidden flex items-center justify-center bg-black">
+                {/* Mobile arrows */}
+                <button
+                  className="sm:hidden absolute left-2 top-1/2 -translate-y-1/2 text-white text-4xl z-10 p-2 bg-black/40 rounded-full"
+                  onClick={prevImage}
+                  aria-label="Previous"
+                  type="button"
+                >
+                  &#8592;
+                </button>
                 <Image
                   src={`/images/projects/${projectType}/${images[currentIdx]}`}
                   alt={images[currentIdx]}
                   fill
-                  className="object-cover "
+                  className="object-cover"
                   sizes="95vw"
                 />
+                <button
+                  className="sm:hidden absolute right-2 top-1/2 -translate-y-1/2 text-white text-4xl z-10 p-2 bg-black/40 rounded-full"
+                  onClick={nextImage}
+                  aria-label="Next"
+                  type="button"
+                >
+                  &#8594;
+                </button>
               </div>
+              {/* Right Arrow */}
               <button
-                className="text-black text-5xl ml-4 z-10 p-2 hover:bg-black/30 rounded-full"
+                className="hidden sm:block text-black text-5xl ml-4 z-10 p-2 hover:bg-black/30 rounded-full"
                 onClick={nextImage}
                 aria-label="Next"
                 type="button"
